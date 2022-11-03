dcee45a27053974046d8485e69cb34bc863a2252
MohamedKharchouf <mohamed.kharchouf@alumni.univ-avignon.fr>
2022-11-03 15:58:49 +0100

    Changement du schema de la BDD, nous en somme à la v1. Ajout de la table, utilisateur et favoris. Ajout des routes permettant de sinscrire, se connecter, recuperer les genres musicales, recuperer les album par genre, ajouter un favoris, supprimer un favoris, recuperer les favoris dun utilisateur et recuperer les informations dun utilisateur. Ajout de certains tests dintegration pour les routes

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -2,9 +2,17 @@ const express = require('express');
 const controllers = require('../controllers/controllers');
 const  router = express.Router();
 
-router.get('/albums',controllers.getAllbums);
+router.get('/albums',controllers.getAlbums);
 router.get('/artistes', controllers.getArtistes);
 router.get('/chansons', controllers.getChansons);
+router.get('/inscrireUtilisateur', controllers.registerUser);
+router.get('/connexionUtilisateur', controllers.userAuthentication);
+router.get('/genreMusical', controllers.getMusicalGenre);
+router.get('/albumGenre', controllers.getAlbumsByGenre);
+router.get('/ajouterFavoris', controllers.addFavorite);
+router.get('/supprimerFavoris', controllers.deleteFavorite);
+router.get('/favorisUtilisateur', controllers.getFavoritesByUser);
+router.get('/informationUtilisateur', controllers.getUserInformation);
 
 
 module.exports = router;
--- schema/scriptSchema_v0.sql
+++ schema/scriptSchema_v0.sql
@@ -1,4 +1,4 @@
-use e_com;
+use vinyle;
 CREATE TABLE artiste (
     id_artiste int not null AUTO_INCREMENT,
     PRIMARY KEY (id_artiste),
