9cd08942bab7924bf6c5961ce2541aa18a7a3b94
imanehacen <mmisipm2>
2022-12-20 00:20:05 +0100

    petite modif du serveur, car erreur coté favoris

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "vinyle"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
   });
 
 connection.connect(function(err) {
