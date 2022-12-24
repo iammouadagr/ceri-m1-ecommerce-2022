7dadd78b614e7b0b1dc454e24ce71021878ed680
Mouad Aguirar <mouad.aguirar@alumni.univ-avignon.fr>
2022-12-24 23:04:41 +0100

    feat(deploy): add frontend cloud run resource

--- main.tf
+++ main.tf
@@ -13,3 +13,22 @@ provider "google" {
     region  = "europe-west1"
 
 }
+
+data "google_secret_manager_secret" "address" {
+  secret_id = "mysql/address"
+}
+
+resource "google_cloud_run_service" "front" {
+  name     = "cloud-run-frontend"
+  location = "europe-west1"
+  max_instances = 1
+
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/blackcat/frontend-app:v1.1.0"
+      }
+    }
+  }
+}
