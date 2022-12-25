dbf8e7555d08f83ee214fcd5730ee1f0b452b683
Mouad Aguirar <mouad.aguirar@alumni.univ-avignon.fr>
2022-12-26 00:06:36 +0100

    feat(deploy): update main.tf

--- main.tf
+++ main.tf
@@ -14,21 +14,78 @@ provider "google" {
 
 }
 
-data "google_secret_manager_secret" "address" {
-  secret_id = "mysql/address"
+data "google_secret_manager_secret" "host" {
+  secret_id = "mysql-address"
 }
 
-resource "google_cloud_run_service" "front" {
+data "google_secret_manager_secret" "user" {
+  secret_id = "mysql-user-blackcat"
+}
+
+data "google_secret_manager_secret" "password" {
+  secret_id = "mysql-password-blackcat"
+}
+
+data "google_secret_manager_secret" "database" {
+  secret_id = "mysql-database-blackcat"
+}
+
+
+resource "google_cloud_run_service" "backend" {
   name     = "cloud-run-frontend"
   location = "europe-west1"
-  max_instances = 1
 
 
   template {
     spec {
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/blackcat/frontend-app:v1.1.0"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/blackcat/backend-app:v1.1.0"
+        env {
+          name = "DB_HOST"
+          value_from {
+            secret_key_ref{
+              name = data.google_secret_manager_secret.a.host
+              key = "latest"
+            }
+          }
+        }
+        env {
+          name = "DB_USER"
+          value_from {
+            secret_key_ref{
+              name = data.google_secret_manager_secret.user.secret_id
+              key = "latest"
+            }
+          }
+        }
+        env {
+          name = "DB_PASSWORD"
+          value_from {
+            secret_key_ref{
+              name = data.google_secret_manager_secret.password.secret_id
+              key = "latest"
+            }
+          }
+        }
+        env {
+          name = "DB_NAME"
+          value_from {
+            secret_key_ref{
+              name = data.google_secret_manager_secret.database.secret_id
+              key = "latest"
+            }
+          }
+        }
       }
     }
   }
+
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+
 }
+
