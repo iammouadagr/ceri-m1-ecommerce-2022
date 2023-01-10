27108b7512d4f048f76fa53ac45288630d2724c0
Mouad Aguirar <mouad.aguirar@alumni.univ-avignon.fr>
2023-01-11 00:50:49 +0100

    refactor(frontend): update env variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL:"https://blackcat-backend-mwjszocsqa-ew.a.run.app/api/v1",
+  enableDebug : false
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL:"http://127.0.0.1:8080/api/v1"
+  API_URL:"https://blackcat-backend-mwjszocsqa-ew.a.run.app/api/v1"
 };
 
 /*
